570116853641d7e8555c5a3ae05270adb6d7da9f
veraAlan <93807967+veraAlan@users.noreply.github.com>
2022-06-08 06:49:08 -0300

    Header
    Complete overhaul.
    Creating header from scratch again.
    TODO: Add style, line up items and give dropdwn function.

--- TP Final/js/main.js
+++
@@ -1,9 +0,0 @@
-/* Toggle responsive on class nav-bar */
-function myFunction() {
-    var page = document.getElementById("top-bar");
-    if (page.className === "nav-bar") {
-        page.className += " responsive";
-    } else {
-        page.className = "nav-bar";
-    }
-}
