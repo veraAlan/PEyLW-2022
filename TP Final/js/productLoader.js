0ee6f4d4426ff4b956adf514263dc15243b19951
veraAlan <alan.vera@est.fi.uncoma.edu.ar>
2022-06-13 19:55:01 -0300

    Bootstrap
    Added map files for bootstrap solving some warnings in console.
    Started working with json file and products info.

--- TP Final/js/productLoader.js
+++ TP Final/js/productLoader.js
@@ -1,6 +1,20 @@
-function productInfo() {
+async function productInfo() {
+    /* Getting document and product-page node */
     const d = document
     const p = d.querySelector("product-page")
+    /* Getting information from db in json style */
+    /* const requestURL = "../json/products"
 
-    console.log(p)
+    const request = new Request(requestURL)
+
+    const response = await fetch(request)
+
+    const pDBText = await response.text()
+
+    const pDB = JSON.parse(pDBText)
+
+    console.log(request)
+    console.log(response)
+    console.log(pDBText)
+    console.log(pDB) */
 }
